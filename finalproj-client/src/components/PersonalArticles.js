92bbf4653e888cd47b9394a13bf84bc46d2d9197
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-12-16 10:44:16 -0500

    able to grab user categories and make an api call

--- finalproj-client/src/components/PersonalArticles.js
+++ finalproj-client/src/components/PersonalArticles.js
@@ -8,7 +8,7 @@ class PersonalArticles extends Component {
     this.state = {
       userid: "",
       merges: [],
-      categoryids: [1, 2, 3, 4, 5, 6],
+      usercategories: [],
       categorynames: [
         "business",
         "entertainment",
@@ -18,31 +18,44 @@ class PersonalArticles extends Component {
         "technology"
       ]
     };
+    this.getArticles = this.getArticles.bind(this);
+    this.getUserArticles = this.getUserArticles.bind(this);
   }
   async componentDidMount() {
     // console.log("personal articles loaded");
-    console.log(this.props.merges);
+    // console.log(this.props.merges);
     const userid = localStorage.getItem("currentuser");
     await this.setState({
       userid: userid,
       merges: this.props.merges
     });
-    console.log(this.state);
-    const matching = this.state.merges.map(merge => {
-      this.state.categoryids.filter((index, name) => {
-        if (merge.category_id === index + 1) {
-          return name;
+    // console.log(this.state);
+    let userCategories = [];
+    this.state.merges.map(merge => {
+      this.state.categorynames.filter((category, index) => {
+        if (merge == index + 1) {
+          userCategories.push(category);
+          // console.log(userCategories);
         } else {
           return "not a match";
         }
       });
     });
-    console.log(matching);
+    this.setState({ usercategories: userCategories });
+    console.log(this.state.usercategories);
+    this.getUserArticles();
+  }
+  getUserArticles() {
+    console.log(this.state.usercategories);
+    this.state.usercategories.map(category => {
+      this.getArticles(category);
+    });
   }
   async getArticles(category) {
     const response = await axios.get(
       `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=92c19aec34bd461db5c18ce60b4a9433 `
     );
+    console.log(response);
   }
   render() {
     return (
